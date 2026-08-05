09b3ded99cc3d6c482aeb7413a8af0fba86a7981
ToujoursPareil8 <jasonaridy@gmail.com>
2026-08-05 11:03:11 +0200

    feat: delete 1 file, create 9 files and update 1 file initialize React project with Vite, TailwindCSS, and ESLint
    - Added package.json with project metadata and dependencies
    - Created main application component (App.jsx) with basic structure and styling
    - Added global CSS styles for consistent theming and layout
    - Set up entry point (main.jsx) for rendering the application
    - Configured Vite for development with React and TailwindCSS support

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import { zap } from "lucide-react";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-4xl text-red-500">Mon super projet React !!!</h1>
+      <h2 className="text-2xl text-blue-500">Coucou</h2>
+      <zap className="w-6 h-6 text-purple-500" />
+    </>
+    
+  );
+}
+
+export default App;
