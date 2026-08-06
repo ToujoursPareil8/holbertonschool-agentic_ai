8dc7d61f6b925d7dfeec5b04fd4f16f8322b6fbf
ToujoursPareil8 <jasonaridy@gmail.com>
2026-08-06 09:28:22 +0200

    feat: update package dependencies and enhance App and Header components for improved layout

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,17 @@ function App() {
     <div className="min-h-screen bg-slate-50">
       <Header />
       <main className="pt-20">
+        {/*
         <Hero />
         <About />
         <Features />
         <Insights />
         <Contact />
+        <Footer />
+        <h1 className="text-4xl text-red-500">Mon super projet React !!!</h1>
+        <h2 className="text-2xl text-blue-500">Coucou</h2>
+        */}
       </main>
-      <Footer />
-      <h1 className="text-4xl text-red-500">Mon super projet React !!!</h1>
-      <h2 className="text-2xl text-blue-500">Coucou</h2>
     </div>
     
   );
