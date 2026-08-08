89e9e307f6bf0dadf12865cd11aec5c9b919cf45
ToujoursPareil8 <jasonaridy@gmail.com>
2026-08-08 16:21:49 +0200

    feat: add Contact section with form handling and validation

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import Header from "./components/Header";
 import Hero from "./sections/Hero";
 import About from "./sections/About";
 import Features from "./sections/Features";
-import Insights from "./sections/Insights"
+import Insights from "./sections/Insights";
+import Contact from "./sections/Contact"
 
 
 function App() {
@@ -15,8 +16,9 @@ function App() {
         <About />
         <Features />
         <Insights />
-        {/*
         <Contact />
+        {/*
+        
         <Footer />
         */}
       </main>
