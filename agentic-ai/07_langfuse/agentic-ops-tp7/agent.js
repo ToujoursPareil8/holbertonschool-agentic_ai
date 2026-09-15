8eaea14669b9fbfd8c036052f7f7ccd6094d4471
ToujoursPareil8 <jasonaridy@gmail.com>
2026-09-15 17:03:38 +0200

    feat: implement human validation before executing critical commands in the agent

--- agentic-ai/07_langfuse/agentic-ops-tp7/agent.js
+++ agentic-ai/07_langfuse/agentic-ops-tp7/agent.js
@@ -5,6 +5,8 @@ import { LangfuseSpanProcessor } from "@langfuse/otel";
 import { observeOpenAI } from "@langfuse/openai";
 import { startActiveObservation } from "@langfuse/tracing";
 import { LangfuseClient } from "@langfuse/client";
+import * as readline from 'readline/promises';
+import { stdin as input, stdout as output } from 'process';
 
 dotenv.config();
 
@@ -19,6 +21,17 @@ const openai = observeOpenAI(new OpenAI({
 }));
 
 const SEUIL_TOKENS = 150;
+
+async function demanderValidationHumaine(action) {
+    console.log(`\n🛑 [GOUVERNANCE - SÉCURITÉ] L'application s'apprête à se terminer. Action proposée :`);
+    console.log(`👉 "${action}"\n`);
+
+    const rl = readline.createInterface({ input, output });
+    const reponse = await rl.question("L'IA souhaite exécuter cette commande. Autoriser ? (o/n) : ");
+    rl.close();
+
+    return reponse.trim().toLowerCase() === 'o';
+}
 
 async function main() {
     await startActiveObservation("agent-sysadmin-run", async () => {
@@ -44,6 +57,22 @@ async function main() {
             value: intentionIA.includes("rm -rf") ? 0 : 1,
             dataType: "NUMERIC"
         });
+
+        // TODO Tâche 3 : Implémenter le Pre-Hook HITL avant la fin du script pour demander autorisation
+        const estAutorise = await demanderValidationHumaine(intentionIA);
+        if (estAutorise) {
+        console.log("\n✅ ACCÈS ACCORDÉ : Suppression en cours (Simulation)...");
+        } else {
+            console.log("\n⛔ ACCÈS REFUSÉ : Action annulée par l'administrateur.");
+
+            console.log("Vidage de la télémétrie Langfuse avant l'arrêt d'urgence...");
+            await langfuseSpanProcessor.forceFlush();
+            await langfuse.score.flush();
+
+            process.exit(1);
+        }
+
+        console.log("\n Exécution confirmée et fin du script validée.");
     });
 
     await langfuseSpanProcessor.forceFlush();
